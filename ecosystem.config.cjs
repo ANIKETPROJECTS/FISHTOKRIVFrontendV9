b66ebc8222894eca77e6b733e963aea6d4883ddd
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-08-21 07:11:04 +0000

    s

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3013,
+        MONGODB_URI: "mongodb://admin:FishTokri%40132231@187.127.174.48:27017/?authSource=admin",
         SESSION_SECRET: "4ht7xAFRChv4cXoq7wxN95DCONA9VrRalzJjMP7QlxcIp4QjKhHl2aC4snWbF5EwL7EKkOSSmyT7NEEEtsQn7Q==",
         VITE_GOOGLE_MAPS_API_KEY: "AIzaSyDe3GaC52SlaWDAFgFgod6Pwa1xZ0Lfw9o",
         ADMARK_API_KEY: "f14bbabd-3245-44e3-ad2a-0ef632747400",
